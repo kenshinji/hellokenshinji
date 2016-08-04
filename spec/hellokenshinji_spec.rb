54eaabc96bcbcc4306008d89ad6011cf2a94a2a3
kenshinji <gundam0083ster@gmail.com>
2016-08-04 01:02:29 +0000

    update hellokenshinji_spec.rb

--- spec/hellokenshinji_spec.rb
+++ spec/hellokenshinji_spec.rb
@@ -4,6 +4,6 @@ require 'hellokenshinji'
 describe Hellokenshinji, type: :helper do
 	let(:extended_class){Class.new {extend Hellokenshinji}}
 	it "greeting from kenshinji" do
-		expect(extended_class.greeting).to eq 0
+		expect(extended_class.greeting).to eq "Hello from kenshinji!"
 	end
 end
